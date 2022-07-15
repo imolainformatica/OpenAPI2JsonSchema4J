23445df135d277068aa96ed2964fe8394bca3361
gcornacchia <gcornacchia@imolainformatica.it>
2022-07-15 15:20:45 +0200

    aggiunto test per oas3

--- src/main/java/it/imolainformatica/openapi2jsonschema4j/base/BaseJsonSchemaGenerator.java
+++ src/main/java/it/imolainformatica/openapi2jsonschema4j/base/BaseJsonSchemaGenerator.java
@@ -4,22 +4,16 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
-import io.swagger.models.Model;
-import io.swagger.models.Path;
 import io.swagger.models.Swagger;
-import io.swagger.models.parameters.BodyParameter;
 import io.swagger.parser.OpenAPIParser;
-import io.swagger.parser.SwaggerParser;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import lombok.Getter;
@@ -28,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BaseJsonSchemaGenerator {
 
+	public static final String APPLICATION_JSON = "application/json";
 	@Getter
 	protected Map<String, JsonNode> generatedObjects = new HashMap<String, JsonNode>();
 	protected static final String ORIGINAL_REF = "originalRef";
@@ -61,21 +56,14 @@ public class BaseJsonSchemaGenerator {
 			log.info(k + "=" + v);
 			for (Operation op : v.readOperations()) {
 				log.info("Operation={}", op.getOperationId());
-				getRequestBodySchema(op, messageObjects);
-				//Optional<Parameter> body = op.getParameters().stream().filter(c -> "body".equalsIgnoreCase(c.getIn()))
-				//		.findFirst();
-				//if (body.isPresent()) {
-					//BodyParameter bp = (BodyParameter) body.get()
-
-
-				//}
+				findRequestBodySchema(op, messageObjects);
 				for (String key : op.getResponses().keySet()){
 					ApiResponse r = op.getResponses().get(key);
 					if (r.getContent()!=null) {
-						if (r.getContent().get("application/json") != null) {
-							if (r.getContent().get("application/json").getSchema().get$ref() != null) {
-								log.info("code={} responseSchema={}", key, r.getContent().get("application/json").getSchema().get$ref());
-								messageObjects.add(r.getContent().get("application/json").getSchema().get$ref());
+						if (r.getContent().get(APPLICATION_JSON) != null) {
+							if (r.getContent().get(APPLICATION_JSON).getSchema().get$ref() != null) {
+								log.info("code={} responseSchema={}", key, r.getContent().get(APPLICATION_JSON).getSchema().get$ref());
+								messageObjects.add(r.getContent().get(APPLICATION_JSON).getSchema().get$ref());
 							} else {
 								log.warn("code={} response schema is not a referenced definition! type={}", key, r.getContent().get("application/json").getClass());
 							}
@@ -106,12 +94,21 @@ public class BaseJsonSchemaGenerator {
 
 	}
 
-	private void getRequestBodySchema(Operation op, Set<String> messageObjects) {
+	private void findRequestBodySchema(Operation op, Set<String> messageObjects) {
 		if (op.getRequestBody()!=null) {
-			Schema sc = op.getRequestBody().getContent().get("application/json").getSchema();
-			if (sc!=null) {
-				log.info("Request schema={}", sc.get$ref());
-				messageObjects.add(sc.get$ref());
+			if (op.getRequestBody().getContent().get(APPLICATION_JSON)!=null) {
+				Schema sc = op.getRequestBody().getContent().get(APPLICATION_JSON).getSchema();
+				if (sc != null) {
+					log.info("Request schema={}", sc.get$ref());
+					if (sc.get$ref()!=null) {
+						messageObjects.add(sc.get$ref());
+					} else {
+						log.warn("Request schema is not a referenced definition!");
+					}
+
+				}
+			} else {
+				log.info("No application body of type 'application/json' found for operation {}",op.getOperationId());
 			}
 		}
 	}
